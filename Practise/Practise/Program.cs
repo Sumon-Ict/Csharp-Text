e4ad317af5be21c644887bd102c1d68c2bbd3854
sumon <sumoniceiu1415@gmail.com>
2020-10-22 22:20:36 +0600

    added method overloading file

---
+++ Practise/Practise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            person p = new person();
+
+            p.Name = "sumon";
+            p.weight = 12.34;
+            p.height = 34.56;
+            p.age = 12;
+
+            p.result("sumon", "suojm");
+
+            p.result(123.34, 45);
+            p.result(34.56, 67.8);
+
+
+            p.result();
+
+
+
+        }
+    }
+}
---
+++ Practise/Practise/person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practise
+{
+    public class person
+    {
+
+        private string name;
+        public double weight;
+        public int age;
+
+        public double height;
+
+
+        public string Name { get; set; }
+
+
+        public void result()
+        {
+            var r = weight * height;
+            Console.WriteLine($"the multiplicatin of the height and weight is {r}");
+
+        }
+
+        public void result(double a,double b)
+        {
+            var r = a * b;
+            Console.WriteLine($"the multiplication od the two number is {r}");
+
+        }
+
+        public void result(double a, int b)
+        {
+            var r = a * b;
+            Console.WriteLine($"the multiplication of the two number is {r}");
+
+        }
+        public void result(string s,string s2)
+        {
+            var r = string.Concat(s, s2);
+            Console.WriteLine($"the concatanatoin of the two string is {r}");
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
