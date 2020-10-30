171fa9213b746f7047a105fc4e9fa295f3087b0c
sumon <sumoniceiu1415@gmail.com>
2020-10-31 00:32:13 +0600

    added files

---
+++ Codeforce/Codeforce/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var testcase = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0;i<testcase;i++)
+            {
+                var b = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+                var n = b[0];
+                var m = b[1];
+
+                var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+               
+
+                
+
+
+
+                if (a.Sum() == m)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+
+
+
+            }
+
+           
+
+
+        }
+    }
+}
