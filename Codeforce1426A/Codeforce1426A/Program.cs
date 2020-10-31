6517d6a16d5ad913b72e6e6ee68d041b6f823bf6
sumon <sumoniceiu1415@gmail.com>
2020-10-31 17:59:45 +0600

    added codeforce Code

---
+++ Codeforce1426A/Codeforce1426A/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Codeforce1426A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testcase = Convert.ToInt32(Console.ReadLine());
+
+
+            for(int i=0;i<testcase;i++)
+            {
+                var b = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+                double x = b[0];
+                double n = b[1];
+
+                double result = 1;
+
+                if (x <= 2)
+                    Console.WriteLine(result);
+                else
+                {
+
+                    var f = x - 2;
+                    double d = f / n;
+
+                    result = Math.Ceiling(d)+1;
+
+                    Console.WriteLine($"{result.ToString("0") }");
+
+
+
+                }
+
+            }
+
+
+
+        }
+    }
+}
---
+++ Codeforce1433/Codeforce1433/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Codeforce1433
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var testcase = Convert.ToInt32(Console.ReadLine());
+
+            for(var i=0;i<testcase;i++)
+            {
+
+
+                long result;
+                var str = Console.ReadLine();
+
+
+                int digit = (str[0]-'0') - 1;
+
+               
+
+                result = digit * 10;
+
+               
+
+
+                if (str.Length == 1)
+                {
+                    result = result + 1;
+
+                }
+                else if (str.Length == 2)
+                {
+                    result = result + 3;
+                }
+                else if (str.Length == 3)
+                    result = result + 6;
+                else
+                    result = result + 10;
+
+                Console.WriteLine(result);
+
+
+
+            }
+        }
+    }
+}
