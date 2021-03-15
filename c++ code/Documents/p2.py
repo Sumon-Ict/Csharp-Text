79fc67018d940de2ac8e6caf2fc754975213dfb5
sumon <sumoniceiu1415@gmail.com>
2021-03-15 11:49:33 +0600

    added c++code

---
+++ c++ code/Documents/p2.py
@@ -0,0 +1,46 @@
+import random
+
+
+
+
+
+
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+x={"salam","kalam","hatim"}
+y={"salam","kalam","kjhakk","karim"}
+z=x.difference(y)
+print(z)
+
+x.difference_update(y)
+
+print(x)
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+x=thisdict["brand"]
+print(x)
+thisdict["year"]=200
+print(thisdict)
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.items()
+
+print(x)
+
+
+
+
+
+
